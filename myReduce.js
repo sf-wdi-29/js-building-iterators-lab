b8fc7be76d8a3076cac2b599ebb9ca1c49cf5b68
Alivia Blount <aliviablount@gmail.com>
2016-09-12 16:36:56 -0700

    In class callback and iterator practice

--- myReduce.js
+++ myReduce.js
@@ -1,10 +1,23 @@
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce for more details
 // Don't worry about initialValue at first. You can always add it in later.
 
-function myReduce(arr, callback) {
+function myReduce(arr, callback, initialValue) {
 
-//  CODE INSIDE HERE   //
+  //  CODE INSIDE HERE   //
+  var previousValue = arr[0];
+  var currentValue = arr[1];
+  var currentIndex = 1
+  var total = 0;
 
+
+  for(var i = 0; i < arr.length; i++){
+    callback(previousValue, currentIndex, currentValue, arr)
+    total = previousValue + currentValue;
+    previousValue = arr[currentIndex + 1]
+    currentValue = arr[currentIndex + 2]
+    currentIndex += 1
+  }
+  return total;
 }
 
 /*
