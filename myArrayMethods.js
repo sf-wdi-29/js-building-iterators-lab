191a4094707c85fc2d4fd2b119390cd5ab499acd
brikky <brandon.kerr160@topper.wku.edu>
2016-09-12 17:40:53 -0700

    finishes homework and bonus methods

--- myArrayMethods.js
+++ myArrayMethods.js
@@ -11,13 +11,13 @@ Array.prototype.myEach = function(callback, thisArg = this) {
 Array.prototype.myMap = function(callback, thisArg = this) {
         returnArray = [];
         for (var i = 0; i < thisArg.length; i++) {
-            returnArray.push(callback(thisArg[i]), i, thisArg);
+            returnArray.push(callback(thisArg[i], i, thisArg));
         }
         return returnArray;
     }
     //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
 
-    Array.prototype.myReduce = function(callback, returnValue = this[0]) {
+Array.prototype.myReduce = function(callback, returnValue = this[0]) {
 
         for (var i = arguments.length > 1 ? 0 : 1; i < this.length; i++) {
             returnValue = callback(returnValue, this[i], i, this);
@@ -26,17 +26,36 @@ Array.prototype.myMap = function(callback, thisArg = this) {
     }
     //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
 
-Array.prototype.myFilter = function(callback) {
-
+Array.prototype.myFilter = function(callback, thisArg = this) {
+        returnArray = [];
+        for (var i = 0; i < thisArg.length; i++) {
+            if (callback(thisArg[i], i, thisArg)) {
+                returnArray.push(thisArg[i]);
+            }
+        }
+        return returnArray;
     }
     //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
 
-Array.prototype.mySome = function(callback) {
-
+Array.prototype.mySome = function(callback, thisArg = this) {
+    for (var i = 0; i < thisArg.length; i++) {
+        if (callback(thisArg[i], i, thisArg)) {
+            return true;
+        }
     }
-    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some
+    return false;
+}
 
-Array.prototype.myEvery = function(callback) {
+}
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some
 
+Array.prototype.myEvery = function(callback, thisArg = this) {
+    for (var i = 0; i < thisArg.length; i++) {
+        if (!callback(thisArg[i], i, thisArg)) {
+            return false;
+        }
     }
-    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every
+    return true;
+}
+}
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every
