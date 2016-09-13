9ae9269d12617c3cb282a0f20addb1acc242ad5b
ramanmehat16 <ramanmehat@gmail.com>
2016-09-12 18:09:13 -0700

    adding myMap, myEach,myReduce

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 var myEach = require('./myEach');
-var myMap = require('./myEach');
-var myReduce = require('./myEach');
+var myMap = require('./myMap');
+var myReduce = require('./myReduce');
 /* *********************************************************************
   You can edit this file
   It will make use of your code in myEach.js, myMap.js and myReduce.js
   To run it on the console do: `node index.js`
 ***********************************************************************/
 
-var numArray = [0,1,10,100,1000];
-
 
 /* myEach */
 
 //
-// myEach(numArray, function print(element, index, arr) {
+// myEach(numArray, function print(eleme  nt, index, arr) {
 //   console.log('inside myEach', element, index, arr);
 // });
 
