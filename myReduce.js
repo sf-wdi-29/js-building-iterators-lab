9231c12c58ad3423f7f3cab51ad18c695df77890
Natalia Hess <nhess@natalia-hesss-macbook-pro.local>
2016-09-12 17:01:34 -0700

    creates myEach myMap myReduce functions

--- myReduce.js
+++ myReduce.js
@@ -1,11 +1,25 @@
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce for more details
 // Don't worry about initialValue at first. You can always add it in later.
 
-function myReduce(arr, callback) {
+function myReduce(arr, callback, initialValue) {
+  var i = 0;
+  var previousValue;
 
-//  CODE INSIDE HERE   //
+  if (initialValue === undefined) {
+    i++;
+    previousValue = arr[0];
+  } else {
+    previousValue = initialValue;
+  }
 
+  for (; i < arr.length; i++) {
+    previousValue = callback(previousValue, arr[i], i, arr);
+  }
+  return previousValue;
 }
+
+
+
 
 /*
  Best if you don't code out here.
